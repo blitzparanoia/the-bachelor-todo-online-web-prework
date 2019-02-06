216831c51974276cf48699abdb66dcf8db263d00
Irene Saez <bluemilk44@gmail.com>
2019-02-06 22:28:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ def get_average_age_for_season(data, season)
   age_total = 0
   number_of_contestants = 0
   data[season].each do |contestant_hash|
-    age_total = (contestant_hash["age"]).to_i
+    age_total += (contestant_hash["age"]).to_i
     number_of_contestants += 1
   end
   (age_total / number_of_contestants.to_f).round(0)
